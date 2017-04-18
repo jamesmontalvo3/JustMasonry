012a53967ec9dde182fe9e51a8beef24b848d119
James Montalvo <edwin.j.montalvo@nasa.gov>
2017-04-18 17:48:06 -0500

    Switch to using OutputPage::addScript() to load masonry

--- JustMasonry.class.php
+++ JustMasonry.class.php
@@ -2,6 +2,10 @@
 
 class JustMasonry {
 	public static function setup ( &$out, &$skin ) {
-		$out->addModules( 'ext.justmasonry' );
+		// $out->addModules( 'ext.justmasonry' );
+
+		global $wgServer, $wgExtensionAssetsPath;
+		$scriptURL = "$wgServer/$wgExtensionAssetsPath/JustMasonry/masonry.pkgd.js";
+		$out->addScript( "<script type='text/javascript' src='$scriptURL'></script>" );
 	}
 }
